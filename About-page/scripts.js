cf644427c15a4ecf1cf58bd9553a3852944ffe5a
Faiz Shaikh <94229675+Faiz-Shaikh-001@users.noreply.github.com>
2023-10-19 00:40:32 +0530

    Delete About-page directory

--- About-page/scripts.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("mobile-menu").style.height = "100%";
-}
-
-function closeNav() {
-    document.getElementById("mobile-menu").style.height = "0";
-}
